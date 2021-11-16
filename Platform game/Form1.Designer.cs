78027a4aa8afd1b1b188cd14b4d12ccb0dabe7ea
Junghans.A <Junghans.A@GERR105P05>
2021-11-16 14:52:18 +0100

    Projektdateien hinzufügen.

---
+++ Platform game/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Platform_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Platform = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.txtScore = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Platform)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Platform
+            // 
+            this.Platform.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Platform.Location = new System.Drawing.Point(3, 416);
+            this.Platform.Name = "Platform";
+            this.Platform.Size = new System.Drawing.Size(1000, 22);
+            this.Platform.TabIndex = 0;
+            this.Platform.TabStop = false;
+            this.Platform.Tag = "platform";
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Player.Location = new System.Drawing.Point(12, 344);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(37, 75);
+            this.Player.TabIndex = 1;
+            this.Player.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Location = new System.Drawing.Point(680, 28);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(35, 13);
+            this.txtScore.TabIndex = 2;
+            this.txtScore.Text = "Score";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox.Location = new System.Drawing.Point(511, 377);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(26, 42);
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Tag = "platformFront";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.Platform);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Platform)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer GameTimer;
+        public System.Windows.Forms.PictureBox Platform;
+        private System.Windows.Forms.Label txtScore;
+        public System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Platform game/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Platform_game
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft, goRight, jumping, GameOver;
+
+        int JumpSpeed;
+        int force;
+        int Score = 0;
+        int PlayerSpeed = 10;
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+
+            Player.Top += JumpSpeed;
+            
+            if(goLeft == true)
+            {
+                Player.Left -= PlayerSpeed;
+                
+            }
+
+            if(goRight == true)
+            {
+                Player.Left += PlayerSpeed;
+            }
+
+            if (jumping == true && force < 0)
+            {
+                jumping = false; 
+            }
+
+            if(jumping == true)
+            {
+                JumpSpeed -= 7;
+                force -= 2;
+            }
+
+            else
+            {
+                JumpSpeed = 15;
+            }
+
+            
+
+            foreach(Control x in this.Controls)
+            {
+                 
+                if(x is PictureBox)
+                {
+                    if((string)x.Tag == "platform")
+                    {
+                        if(Player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            
+                            force = 8;
+                            Player.Top = x.Top - Player.Height;
+
+                        }
+
+                        x.BringToFront();
+                    }
+
+                    if ((string)x.Tag == "platformFront")
+                    {
+                        if (Player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            RestartGame();
+                            MessageBox.Show("Verloren");
+                        }
+                        x.BringToFront();
+                    }
+
+                }
+                if (Player.Left < -150)
+                {
+                    Platform.Left += 800;
+                }
+
+            }
+            
+        }
+
+        
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+        //Wenn A gedrückt, dann Links laufen
+        if(e.KeyCode == Keys.A)
+            {
+                goLeft = true;
+            }
+        //Wenn D gedrückt, dann Rechts laufen
+        if(e.KeyCode == Keys.D)
+            {
+                goRight = true;
+            }
+        //Wenn Leer Taste bereits gedrückt dann Gravitation
+        if(e.KeyCode == Keys.Space && jumping == false)
+            {
+                jumping = true; 
+            }
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                goRight = false;
+            }
+
+            if(jumping == true)
+            {
+                jumping = false; 
+            }
+
+            if(e.KeyCode == Keys.Enter && GameOver == true)
+            {
+                RestartGame();
+            }
+
+
+        }
+
+        private void RestartGame()
+        {
+            jumping = false;
+            goLeft = false;
+            goRight = false;
+            GameOver = false;
+            int Score = 0;
+
+            Convert.ToString(Score);
+
+            txtScore.Text = "Score" + Score;
+
+            foreach(Control x in this.Controls)
+                if(x is PictureBox && x.Visible == false)
+                {
+                    x.Visible = true;
+                }
+
+            Player.Left = 12;
+            Player.Top = 335;
+
+            GameTimer.Start();
+
+        }
+    }
+}
